5998fd44ddd99b6fd9acf23c3af80331d0b236f4
Cliff Pyles <cliff@cliffpyles.com>
2025-12-02 09:37:58 -0500

    Disable sm_90 compilation for Colab A100 builds
    - Comment out sm_90 (H100) compute capability compilation
    - Reduces build time by ~50% (from 120 to 60 effective compilations)
    - Only sm_80 (A100) is now compiled, which is sufficient for Colab A100
    - Build will be faster and use less memory during compilation

--- setup.py
+++ setup.py
@@ -120,10 +120,11 @@ if not SKIP_CUDA_BUILD:
     # cc_flag.append("arch=compute_75,code=sm_75")
     cc_flag.append("-gencode")
     cc_flag.append("arch=compute_80,code=sm_80")
-    if CUDA_HOME is not None:
-        if bare_metal_version >= Version("11.8"):
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_90,code=sm_90")
+    # Skip sm_90 compilation - Colab A100 only needs sm_80
+    # if CUDA_HOME is not None:
+    #     if bare_metal_version >= Version("11.8"):
+    #         cc_flag.append("-gencode")
+    #         cc_flag.append("arch=compute_90,code=sm_90")
 
     # HACK: The compiler flag -D_GLIBCXX_USE_CXX11_ABI is set to be the same as
     # torch._C._GLIBCXX_USE_CXX11_ABI
